bd90c2db610018d0f5b1587295fff1c3980adcd2
关签 <buzichang@vip.qq.com>
2016-12-15 21:39:10 +0800

    first commit

---
+++ core/common.js
@@ -0,0 +1,90 @@
+var viewError = false
+
+exports.createPage = function (url, callback, page) {
+	if (page == null) {
+		page = new WebPage();
+		page.clipRect = { top: 0, left: 0, width: 1024, height: 768 };
+		page.viewportSize = { width: 1024, height: 768 };
+		page.settings.resourceTimeout = 8000;
+		page.settings.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36';
+		page.settings.loadImages = false
+
+		page.onError = function(msg, trace) {
+			if (!viewError)
+				return
+			var msgStack = ['ERROR: ' + msg];
+
+			if (trace && trace.length) {
+				msgStack.push('TRACE:');
+				trace.forEach(function(t) {
+					msgStack.push(' -> ' + t.file + ': ' + t.line + (t.function ? ' (in function "' + t.function +'")' : ''));
+				});
+			}
+
+			console.error(msgStack.join('\n'));
+		};
+	}
+
+	page.open(url, callback);
+	page.clearCookies()
+
+	return page
+}
+
+exports.ViewError = function (b) {
+	viewError = b
+}
+
+var requestCount = 0
+
+exports.createLog = function (source, id) {
+	requestCount += 1
+	var count = requestCount
+	return function(message) { console.log("#" + requestCount +  " [" + source + " - " + id + "] " + message) }
+}
+
+exports.errorExit = function(text, code) {
+    console.log(text)
+    phantom.exit(code || 2)
+}
+
+exports.sourceList = [ "nets", "qq" ]
+
+exports.makeFailedData = function(text) {
+	return { 'code': 400, 'err': text || "failed to request" }
+}
+
+exports.makeResponseData = function(dataList) {
+	var merge = {}
+
+	for (var i in dataList) {
+		var e = dataList[i]
+		if (typeof (e) == 'object') {
+			for (var k in e) {
+				merge[k] = e[k]
+			}
+		}
+	}
+
+	var rt = {
+		'version': 1,
+		'id': merge['id'],
+		'name': merge['name'],
+		'tname': merge['tname'] || null,
+		'singer': merge['singer'], // { name: "", id: "" }
+		'lrc': merge['lrc'] || null,
+		'tlrc': merge['tlrc'] || null,
+		// 'mv_id': merge['mv_id'] || null,
+		'album_name': merge['album_name'] || null,
+		'album_id': merge['album_id'] || null,
+		'code': 200,
+	}
+
+	rt['nolyric'] = merge['nolyric'] || (!rt['lrc'] && true || false)
+
+	if (!rt['id'] || !['name'] || !['album_name']) {
+		return makeFailedData('missing base data')
+	}
+
+	return rt
+}
---
+++ core/downloader/ntes.js
@@ -0,0 +1,95 @@
+var common = require(phantom.libraryPath + '/core/common.js')
+
+exports.download = function download_ntes(id, response) {
+	var log = common.createLog('ntes', id)
+	var url = 'http://music.163.com/#/m/song?id=' + id
+	log('open: ' + url)
+	var page = common.createPage(url, function(status) {
+		log('ok')
+		page.onConsoleMessage = function(msg, lineNum, sourceId) {
+			try {
+				log('load lyrics finished, try read song info')
+				var song_info = page.evaluate(function() {
+					var c = window.contentFrame.document.getElementsByTagName('a')
+					var name = null
+					var tname = null
+					var mv_id = null
+					var album_name = null
+					var album_id = null
+					var singer = [ ]
+
+					for (var i=0; i!= c.length; ++i) {
+						if (c[i].getAttribute('data-res-name') != null) {
+							name = c[i].getAttribute('data-res-name')
+						}
+
+						if (!mv_id && (c[i].getAttribute('href') != null) && (c[i].getAttribute('href').indexOf('/mv?id=') != -1)) {
+							mv_id = c[i].getAttribute('href').match(/\d+/)
+							mv_id = mv_id.length > 0 && mv_id[0] || null
+						}
+
+						if (!album_id && (c[i].getAttribute('href') != null) && (c[i].getAttribute('href').indexOf('/album?id=') != -1)) {
+							album_name = c[i].text
+							album_id = c[i].getAttribute('href').match(/\d+/)
+							album_id = album_id.length > 0 && album_id[0] || null
+						}
+
+						if ((c[i].getAttribute('href') != null) && (c[i].getAttribute('href').indexOf('/artist?id=') != -1)) {
+							var author_name = c[i].text
+							var author_id = c[i].getAttribute('href').match(/\d+/)
+							if (author_id.length == 1) {
+								singer.push({ 'name': author_name, 'id': author_id[0] })
+							}
+						}
+					}
+
+					var c = window.contentFrame.document.getElementsByClassName('tit')
+
+					if (c.length > 0) {
+						var c2 = c[0].getElementsByClassName('subtit')
+						if (c2.length > 0) {
+							tname = c2[0].textContent
+						}
+					}
+
+					return {
+						'name': name,
+						'tname': tname,
+						'mv_id': mv_id,
+						'album_name': album_name,
+						'album_id': album_id,
+						'singer': singer
+					}
+				});
+
+				if (song_info == null || song_info == undefined || !song_info['name']) {
+					log('failed read song info')
+					response(common.makeFailedData('failed read song info'))
+				} else {
+					var data = JSON.parse(msg)
+                    log('load lyrics succeed')
+					response(common.makeResponseData([
+						song_info,
+						{
+							'id': id,
+							'lrc': data['lrc'] != null && data['lrc']['lyric'] || null,
+							'tlrc': data['tlyric'] != null && data['tlyric']['lyric'] || null
+						}
+					]))
+				}
+			}
+			catch(e)
+			{
+				response(makeFailedData(e || 'err'))
+			}
+
+			setTimeout(function(){
+				page.close();
+			}, 100)
+		};
+
+		log('load lyrics start')
+
+		page.evaluateJavaScript('(function(){var bd=NEJ.P,bI=bd("nej.ut"),bA=bd("nej.j"),bL=bd("nm.s"),bc,bO;bL.bnA=NEJ.C();bc=bL.bnA.bU(bI.fb);bc.cY=function(){this.df();var bZ="/api/song/lyric",cN={id:#ID#,lv:-1,tv:-1};this.EF=cN.id;bA.cG(bZ,{sync:false,type:"json",query:cN,method:"get",onload:this.bny.bi(this),onerror:this.bny.bi(this)})};bc.bny=function(be){console.log(JSON.stringify(be))};new bL.bnA})'.replace("#ID#", id))
+	})
+}
