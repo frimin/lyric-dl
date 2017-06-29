dba62e742e384a14015baf165e6edfdc18589bb6
关签 <buzichang@vip.qq.com>
2017-06-29 22:45:44 +0800

    add ‘xiami’ support

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var loader = require('./lib/core/loader')
-var command_download = require('./lib/command/download')
+
+// APIs
 
 module.exports.getLoader = function (name) {
-    return loader[name]
+    return loader.all[name]
 }
 
 module.exports.getLoaderFromUrl = function (url) {
-    return command_download.parseUrl(url)
+    return loader.parseUrl(url)
 }
